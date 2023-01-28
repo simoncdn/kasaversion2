9d0fc653ea15403b314510c3764ac09bd4ddb00f
Cardona simon <simon.cdn@icloud.com>
2023-01-28 17:32:27 +0100

    ✨ F05 : PAGES > Add Lodging page

--- src/components/pages/lodging/Lodging.tsx
+++ src/components/pages/lodging/Lodging.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { LodgingIndoProps } from "../../../utils/type";
 
-export default function Lodging() {
-  return <div>Lodging</div>;
+export default function LodgingInfo({
+  title,
+  location,
+  tags,
+  host,
+  rating,
+}: LodgingIndoProps) {
+  return (
+    <div className="lodging-info-container">
+      <div className="lodging-info-left">
+        <h2>{title}</h2>
+        <p>{location}</p>
+
+        <div className="tags-container">
+          <ul>
+            {tags.map((element, index) => (
+              <li key={index} className="tag">
+                {element}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="lodging-info-right">
+        <div className="lodging-info-right-host">
+          <p>{host.name}</p>
+          <img src={host.picture} alt="host img" />
+        </div>
+        <div>
+          <Rating rating={rating} />
+        </div>
+      </div>
+    </div>
+  );
 }
