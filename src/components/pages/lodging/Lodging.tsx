99f570437fa13eab6f63c241b7517f825b008194
Cardona simon <simon.cdn@icloud.com>
2023-01-28 17:47:41 +0100

    ✨ F05 : LODGING > Add reusable component

--- src/components/pages/lodging/Lodging.tsx
+++ src/components/pages/lodging/Lodging.tsx
@@ -1,38 +1,62 @@
-import { LodgingIndoProps } from "../../../utils/type";
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Footer from "../../reusableui/footer/Footer";
+import Header from "../../reusableui/header/Header";
+import lodgingsData from "../../../data/lodging.json";
+import Gallery from "./Gallery";
+import Collapse from "../../reusableui/collapse/Collapse";
+import LodgingInfo from "./LodgingInfo";
 
-export default function LodgingInfo({
-  title,
-  location,
-  tags,
-  host,
-  rating,
-}: LodgingIndoProps) {
+export default function Lodging() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!lodgingsData.find((item) => item.id === id)) {
+      return navigate("/error404");
+    }
+  });
+
   return (
-    <div className="lodging-info-container">
-      <div className="lodging-info-left">
-        <h2>{title}</h2>
-        <p>{location}</p>
+    <div className="lodging-container">
+      <Header />
+      <div className="lodging-content">
+        {lodgingsData
+          .filter((item) => item.id === id)
+          .map((item) => {
+            return (
+              <div key={item.id}>
+                <Gallery images={item.pictures} />
 
-        <div className="tags-container">
-          <ul>
-            {tags.map((element, index) => (
-              <li key={index} className="tag">
-                {element}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+                <LodgingInfo
+                  title={item.title}
+                  location={item.location}
+                  tags={item.tags}
+                  host={{
+                    name: `${item.host.name}`,
+                    picture: `${item.host.picture}`,
+                  }}
+                  rating={item.rating}
+                />
 
-      <div className="lodging-info-right">
-        <div className="lodging-info-right-host">
-          <p>{host.name}</p>
-          <img src={host.picture} alt="host img" />
-        </div>
-        <div>
-          <Rating rating={rating} />
-        </div>
+                <div className="collapses">
+                  <div className="collapse">
+                    <Collapse title="Description" content={item.description} />
+                  </div>
+                  <div className="collapse">
+                    <Collapse
+                      title="Équipements"
+                      content={item.equipments.map((item) => (
+                        <p key={item}>{item}</p>
+                      ))}
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </div>
+      <Footer />
     </div>
   );
 }
