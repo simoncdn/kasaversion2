6d7159ef7bbde6702de882da5c1eb9a82d519e0e
Cardona simon <simon.cdn@icloud.com>
2023-01-28 13:37:31 +0100

    ✨ F02 : REUSABLEUI > Add Footer component

---
+++ src/components/reusableui/footer/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-logo">
+        <img src="/Images/logo.svg" alt="logo Kasa" />
+      </div>
+      <p>© 2020 Kasa. All rights reserved</p>
+    </div>
+  );
+}
