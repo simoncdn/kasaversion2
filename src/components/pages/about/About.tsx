ce3d0542b81f0193cb737ef502f384a27f5282a0
Cardona simon <simon.cdn@icloud.com>
2023-01-28 17:12:35 +0100

    ✨ F03 : PAGES > Add About

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import Banner from "../../reusableui/banner/Banner";
+import Header from "../../reusableui/header/Header";
+import Collapse from "../../reusableui/collapse/Collapse";
+import Footer from "../../reusableui/footer/Footer";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="about-container">
+      <Header />
+      <Banner image={"/Images/bannerImgAbout.png"} title={""} />
+      <div className="collapse-bloc">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+        />
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 }
