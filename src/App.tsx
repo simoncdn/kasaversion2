67af6380c28f050c6d737e8665da2dc20f912c9d
Cardona simon <simon.cdn@icloud.com>
2023-01-28 12:32:59 +0100

    ✨ F01 : APP > add routing path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Lodging from "./pages/lodging/Lodging";
+import Error from "./pages/error/Error";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logements/:id" element={<Lodging />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/logements/*" element={<Error />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
