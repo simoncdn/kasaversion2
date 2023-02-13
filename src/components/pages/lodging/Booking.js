f71f8454e1a80a99eb5c366fd9d70eba8d2ea84e
Cardona simon <simon.cdn@icloud.com>
2023-02-13 22:59:11 +0100

    ✨ V2 : LODGING > Add Booking component

---
+++ src/components/pages/lodging/Booking.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import styled from "styled-components";
+import { BiMailSend } from "react-icons/bi";
+
+export default function Booking({ price }) {
+  const [isUserEmail, setIsUserEmail] = useState("");
+
+  const handleChangeEmail = (event) => {
+    setIsUserEmail(event.target.value);
+  };
+
+  const handleBookingSubmit = (event) => {
+    event.preventDefault();
+
+    toast.info("Email envoyé ✅", {
+      theme: "dark",
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      icon: <BiMailSend />,
+    });
+    setIsUserEmail("");
+  };
+  return (
+    <BookingStyled onSubmit={handleBookingSubmit}>
+      <h3>
+        <span>{price}</span> par nuit
+      </h3>
+      <p>
+        Veuillez renseigner votre adresse mail. <br />
+        L'hôte prendra contact avec vous pour les disponibilités du logement.
+      </p>
+      <div className="booking-input">
+        <input
+          type="email"
+          name="mail"
+          id="mail"
+          placeholder="Entrez votre adresse email"
+          value={isUserEmail}
+          onChange={handleChangeEmail}
+          required
+        />
+        <button>Réserver</button>
+      </div>
+    </BookingStyled>
+  );
+}
+
+const BookingStyled = styled.form`
+  background-color: white;
+  border-radius: 15px;
+  width: 80%;
+  height: 300px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  span {
+    font-weight: 700;
+    font-size: 30px;
+  }
+  h3 {
+    font-weight: 400;
+    font-size: 20px;
+  }
+  .booking-input {
+    display: flex;
+    flex-direction: column;
+    input {
+      height: 50px;
+      padding: 5px;
+      border-radius: 5px;
+      border: 1px solid black;
+      margin-bottom: 15px;
+      font-size: 18px;
+    }
+    button {
+      border: none;
+      background-color: #ff6060;
+      height: 50px;
+      border-radius: 5px;
+      color: white;
+      font-size: 18px;
+      cursor: pointer;
+    }
+  }
+`;
