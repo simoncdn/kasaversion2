83f9c1f75fad4775ae4d49abeebc9f6b5012f2ee
Cardona simon <simon.cdn@icloud.com>
2023-02-13 16:55:09 +0100

    ✨ V2 : APP > Add Footer

---
+++ src/components/reusableui/Footer.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export default function Footer() {
+  return (
+    <FooterStyled>
+      <div className="footer-logo">
+        <img src="/Images/logo.svg" alt="logo Kasa" />
+      </div>
+      <p>© 2020 Kasa. All rights reserved</p>
+    </FooterStyled>
+  );
+}
+
+const FooterStyled = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: #191919;
+  padding: 50px 0px 30px 0px;
+  text-align: center;
+  border-top: 2px solid #ffffff32;
+  .footer-logo img {
+    filter: brightness(10);
+    max-width: 130px;
+  }
+  p {
+    color: white;
+    font-size: 24px;
+    font-weight: 100;
+    margin-top: 30px;
+  }
+
+  @media screen and (max-width: 1024px) {
+  }
+  @media screen and (max-width: 768px) {
+    height: 120px;
+    padding: 30px 0px 20px 0px;
+
+    .footer-logo img {
+      width: 90px;
+    }
+    .footer-container p {
+      font-size: 12px;
+      margin-top: 10px;
+    }
+  }
+`;
